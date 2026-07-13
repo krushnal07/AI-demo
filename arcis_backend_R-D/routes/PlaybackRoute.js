4facce7bccd2eaa2d2729db7ae96a489ab3cb1f2
krushnal07 <krushnalpatil2955@gmail.com>
2026-07-13 17:41:24 +0530

    Delete arcis_backend_R-D/routes/PlaybackRoute.js

--- arcis_backend_R-D/routes/PlaybackRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { getPlayback } = require("../controllers/playbackController");
-
-const router = express.Router();
-
-// Returns a JSON playlist of Azure SAS video URLs for a camera + time window
-router.get("/", getPlayback);
-
-module.exports = router;
