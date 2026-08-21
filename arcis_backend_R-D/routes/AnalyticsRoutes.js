ece63782a73af5bfa28bbc105d471e5824802fd7
krushnal07 <krushnalpatil2955@gmail.com>
2026-08-21 12:57:29 +0530

    Add route for fetching latest alerts

--- arcis_backend_R-D/routes/AnalyticsRoutes.js
+++ arcis_backend_R-D/routes/AnalyticsRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const {
-  saveAnalyticsImage,getAnalyticsImages,getZoneWiseCounts,getAiDashboard // ✅ Correct import
+  saveAnalyticsImage,getAnalyticsImages,getZoneWiseCounts,getAiDashboard,getLatestAlerts // ✅ Correct import
 } = require("../controllers/analyticsimageController"); // ✅ Ensure correct path
 // const { getEmailSettings, updateEmailSettings } = require('../controllers/settingsController');
 //const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
@@ -12,6 +12,7 @@ router.route("/analytics").post(saveAnalyticsImage);
 router.route("/getanalyticsimages").get(getAnalyticsImages);
 router.route("/getZoneWiseCounts").get(getZoneWiseCounts);
 router.route("/ai-dashboard").get(getAiDashboard);
+router.route("/latest-alerts").get(getLatestAlerts);
 // router.get('/settings', getEmailSettings); // ✅ Correct route
 // router.put('/admin/email-settings', updateEmailSettings); // ✅ Correct route
 module.exports = router;
